b064adb73d327302fa0adb3f27f8871eaaeec486
unknown <laravel.amit@gmail.com>
2021-12-07 02:46:27 +0530

    initial commit

---
+++ src/comments/Comments.js
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import CommentForm from "./CommentForm";
+import Comment from "./Comment";
+import {
+  createComment as createCommentApi,
+  updateComment as updateCommentApi,
+  deleteComment as deleteCommentApi,
+} from "../api";
+
+const Comments = ({currentUserId }) => {
+  const [backendComments, setBackendComments] = useState(
+    localStorage.getItem('commentdata') ? JSON.parse(localStorage.getItem('commentdata')) : [] 
+  );
+  const [activeComment, setActiveComment] = useState(null);
+  const rootComments = backendComments.filter(
+    (backendComment) => backendComment.parentId === null
+  );
+  const getReplies = (commentId) =>
+    backendComments
+      .filter((backendComment) => backendComment.parentId === commentId)
+      .sort(
+        (a, b) =>
+          new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+      );
+  const addComment = (text, parentId) => {
+    createCommentApi(text, parentId).then((comment) => {
+      setBackendComments([comment, ...backendComments]);
+      localStorage.setItem('commentdata',JSON.stringify([comment, ...backendComments]));
+      setActiveComment(null);
+    });
+  };
+
+  const updateComment = (text, commentId) => {
+    updateCommentApi(text).then(() => {
+      const updatedBackendComments = backendComments.map((backendComment) => {
+        if (backendComment.id === commentId) {
+          return { ...backendComment, body: text };
+        }
+        return backendComment;
+      });
+      setBackendComments(updatedBackendComments);
+      localStorage.setItem('commentdata',JSON.stringify(updatedBackendComments));
+      setActiveComment(null);
+    });
+  };
+  const deleteComment = (commentId) => {
+    if (window.confirm("Are you sure you want to remove comment?")) {
+      deleteCommentApi().then(() => {
+        const updatedBackendComments = backendComments.filter(
+          (backendComment) => backendComment.id !== commentId
+        );
+        setBackendComments(updatedBackendComments);
+        localStorage.setItem('commentdata',JSON.stringify(updatedBackendComments))
+      });
+    }
+  };
+
+  return (
+    <div className="comments">
+      <h3 className="comments-title">Comments</h3>
+      <div className="comment-form-title">Write comment</div>
+      <CommentForm submitLabel="post" placehoderLabel ="Type your reply here" handleSubmit={addComment} />
+      <div className="comments-container">
+        {rootComments.map((rootComment) => (
+          <Comment
+            key={rootComment.id}
+            comment={rootComment}
+            replies={getReplies(rootComment.id)}
+            activeComment={activeComment}
+            setActiveComment={setActiveComment}
+            addComment={addComment}
+            deleteComment={deleteComment}
+            updateComment={updateComment}
+            currentUserId={currentUserId}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Comments;
